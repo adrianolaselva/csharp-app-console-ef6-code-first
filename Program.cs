00563c7957514239d19319147cf5426ed8b86540
Adriano M. La Selva <adrianolaselva@gmail.com>
2016-11-11 09:24:02 -0200

    Exemplo

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF6CodeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var entity = new Model())
+            {
+                Pessoa person = new Pessoa() { Name = "Adriano La Selva", Email = "adrianolaselva123@gmail.com" };
+
+                person = entity.Pessoa.Add(person);
+
+                Console.WriteLine(person.Id);
+
+                entity.SaveChanges();
+            }
+        }
+    }
+}
